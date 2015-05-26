dda7b836fc7cd4969e7a5a43656376d959590549
QuanZhou <joshzhou16@gmail.com>
2015-05-26 23:28:27 +0200

    add-plugin: hexo-markdown-it

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 hexo clean
 hexo generate
 hexo deploy
-
+echo -e "INFO Changes will be effectives in few seconds.\nINFO Browser will be opened later.\n"
+sleep 10s
 open -a "Google Chrome" http://joshpat.github.io/
