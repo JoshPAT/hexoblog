04beee5d144cec1d0ddfdd5504303112c1d7a047
QuanZhou <joshzhou16@gmail.com>
2015-05-28 21:20:24 +0200

    update deploy check

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,8 @@ hexo clean
 hexo generate
 hexo deploy
 echo -e "INFO Changes will be effectives in few seconds.\nINFO Browser will be opened later.\n"
-sleep 10s
-open -a "Google Chrome" http://joshpat.github.io/
+osascript -e 'tell app "Terminal"
+   do script "cd ~/Documents/hexoblog;hexo server"
+end tell'
+sleep 2s
+open -a "Google Chrome" http://localhost:4000
