511248312797e9d613241826b0a0fce37ca87207
QuanZhou <joshzhou16@gmail.com>
2016-03-18 14:29:11 +0100

    chmod deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 hexo clean
 hexo generate
 hexo deploy
-echo -e "INFO Changes will be effectives in few seconds.\nINFO Browser will be opened later.\n"
-osascript -e 'tell app "Terminal"
-   do script "cd ~/Documents/hexoblog;hexo server"
-end tell'
-sleep 2s
-open -a "Google Chrome" http://localhost:4000
+# echo -e "INFO Changes will be effectives in few seconds.\nINFO Browser will be opened later.\n"
+# osascript -e 'tell app "Terminal"
+#    do script "cd ~/Documents/hexoblog;hexo server"
+# end tell'
+# sleep 2s
+# #open -a "Google Chrome" http://localhost:4000
